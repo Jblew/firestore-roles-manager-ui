0b6f6217a2a1e129f5e4e411e75340c05cef2d56
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 21:47:59 +0200

    feat: setup theme colors

--- src/config/visual-config.ts
+++ src/config/visual-config.ts
@@ -7,7 +7,24 @@ export const visualConfig = {
         display: "Jędrzej Lewandowski <jedrzejblew@gmail.com>",
     },
     colors: {
-        primary: "#00885B",
-        secondary: "#D20738",
+        primary: "#607D8B",
+        darkPrimary: "#455A64",
+        lightPrimary: "#CFD8DC",
+        textOnPrimary: "#FFFFFF",
+        accent: "#FFC107",
+        primaryText: "#212121",
+        secondaryText: "#757575",
+        divider: "#BDBDBD",
     },
 };
+
+/*
+$primary-color-dark:   #455A64
+$primary-color:        #607D8B
+$primary-color-light:  #CFD8DC
+$primary-color-text:   #FFFFFF
+$accent-color:         #FFC107
+$primary-text-color:   #212121
+$secondary-text-color: #757575
+$divider-color:        #BDBDBD
+*/
