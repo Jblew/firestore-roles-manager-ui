a0bc1f05ffea37149340d20d68a78bfe488c21d1
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 20:39:21 +0200

    feat: setup main component

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,46 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import "firebaseui/dist/firebaseui.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css"; // Ensure you are using css-loader
+import "typeface-roboto"; // offline version of roboto font
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+import App from "./App.vue";
+import "./components/common/common_components";
+import "./filters";
+import router from "./router/router";
+import { routes } from "./router/routes";
+import { AuthModule } from "./store/modules/auth/AuthModule";
+import { Actions, s, store } from "./store/store";
 
 Vue.config.productionTip = false;
 
+// Vuetify
+Vue.use(Vuetify);
+
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+    router,
+    store,
+    render: h => h(App),
+    computed: {
+        authState(): AuthModule.AuthState {
+            return s(this.$store).state.auth.state;
+        },
+    },
+    watch: {
+        authState(authState, oldAuthState) {
+            if (authState === AuthModule.AuthState.NOTAUTHENTICATED) {
+                this.$router.push(routes.auth.path);
+            } else if (authState === AuthModule.AuthState.AUTHENTICATED) {
+                // this.$store.dispatch(ListModule.Actions.updateQueryFilterAndReloadList, {});
+            }
+        },
+    },
+    created() {
+        this.$store.dispatch(Actions.initialize);
+    },
+    mounted() {
+        //
+    },
+    methods: {},
+}).$mount("#app");
