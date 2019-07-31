8a1dece5f3e0cc67300d2c6410ca49f3495f272a
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 20:47:38 +0200

    feat: add visual config

---
+++ src/config/visual-config.ts
@@ -0,0 +1,13 @@
+export const visualConfig = {
+    appTitle: "Firestore-roles management app",
+    author: {
+        name: "Jędrzej Lewandowski",
+        email: "jedrzejblew@gmail.com",
+        link: "https://jedrzej.lewandowski.doctor/",
+        display: "Jędrzej Lewandowski <jedrzejblew@gmail.com>",
+    },
+    colors: {
+        primary: "#00885B",
+        secondary: "#D20738",
+    },
+};
