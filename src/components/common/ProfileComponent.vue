f9f7f1290bbce5dfab560c534c38e358e2c72037
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 20:36:22 +0200

    feat: add ProfileComponent

---
+++ src/components/common/ProfileComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div v-if="authenticated">
+    <v-toolbar flat class="transparent">
+      <v-list class="pa-0 pb-0 mb-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img :src="photoUrl" v-if="photoUrl.length > 0">
+            <v-icon v-else>person</v-icon>
+          </v-list-tile-avatar>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ name }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-toolbar flat class="transparent">
+      <v-list class="pa-0">
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-btn small @click="signOut()">{{ text.signOut }}</v-btn>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import { labels, s } from "../../global";
+import { AuthModule } from "../../store/modules/auth/AuthModule";
+
+export default Vue.extend({
+    name: "ProfileComponent",
+    data() {
+        return {
+            text: {
+                signOut: labels.logOut,
+            },
+        };
+    },
+    computed: {
+        authenticated(): boolean {
+            return s(this.$store).state.auth.state === AuthModule.AuthState.AUTHENTICATED;
+        },
+        photoUrl(): string {
+            return s(this.$store).state.auth.profileImageURL || "";
+        },
+        name(): string {
+            return s(this.$store).state.auth.username || "";
+        },
+    },
+    methods: {
+        signOut() {
+            s(this.$store).dispatch(AuthModule.Actions.logout);
+        },
+    },
+});
+</script>
