16fdc8ef433a88767395e261c8fad020c3191135
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 20:49:11 +0200

    feat: add boilerplate for List view

---
+++ src/views/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="advicelist">
+    <h1 class="mb-3 ml-2">List</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import { config, labels, s } from "../global";
+
+export default Vue.extend({
+    data() {
+        return {};
+    },
+    methods: {},
+    components: {},
+});
+</script>
+<style scoped lang="scss">
+</style>
