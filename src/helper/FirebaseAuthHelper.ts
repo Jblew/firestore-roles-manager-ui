c37d3f020ec450f44096a707ea5553b40ebcddb4
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 20:38:09 +0200

    feat: define FirebaseAuthHelper

---
+++ src/helper/FirebaseAuthHelper.ts
@@ -0,0 +1,48 @@
+// tslint:disable:ordered-imports
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import * as firebaseui from "firebaseui";
+import { Configuration } from "@/Configuration";
+
+export class FirebaseAuthHelper {
+    public static initialize(opts: FirebaseAuthHelper.InitializeOptions) {
+        firebase.initializeApp(Configuration.load().firebase);
+        firebase.auth().onAuthStateChanged((user: FirebaseAuthHelper.User | null) => {
+            if (user) {
+                opts.authenticatedCb(user);
+            } else {
+                opts.notAuthenticatedCb();
+            }
+        });
+    }
+
+    public static async signOut() {
+        await firebase.auth().signOut();
+    }
+
+    public static doAuth(): firebase.auth.Auth {
+        return firebase.auth();
+    }
+
+    public static startFirebaseAuthUI(id: string, signInSuccessfulUrl: string, signInProviders: string[]) {
+        const uiConfig = {
+            signInSuccessUrl: signInSuccessfulUrl,
+            signInOptions: signInProviders,
+        };
+        FirebaseAuthHelper.UI_INSTANCE =
+            FirebaseAuthHelper.UI_INSTANCE || new firebaseui.auth.AuthUI(FirebaseAuthHelper.doAuth());
+        FirebaseAuthHelper.UI_INSTANCE.start(id, uiConfig);
+    }
+
+    private static UI_INSTANCE: firebaseui.auth.AuthUI | undefined = undefined;
+}
+
+export namespace FirebaseAuthHelper {
+    export interface InitializeOptions {
+        authenticatedCb: (user: FirebaseAuthHelper.User) => void;
+        notAuthenticatedCb: () => void;
+    }
+
+    export type User = firebase.User;
+}
