a96b2f17cdb102500d3189241ef94c97bfc91e6e
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-31 20:37:58 +0200

    feat: define project Configuration and its loader

---
+++ src/Configuration.ts
@@ -0,0 +1,33 @@
+import { FirestoreRolesConfiguration } from "firestore-roles";
+import ow from "ow";
+
+export interface Configuration {
+    firebase: {
+        apiKey: string;
+        authDomain: string;
+        databaseURL: string;
+        projectId: string;
+    };
+    roles: FirestoreRolesConfiguration;
+}
+
+export namespace Configuration {
+    export function validate(c: Configuration) {
+        ow(c.firebase, "Configuration.firebase", ow.object);
+        ow(c.firebase.apiKey, "Configuration.firebase.apiKey", ow.string.nonEmpty);
+        ow(c.firebase.authDomain, "Configuration.firebase.authDomain", ow.string.nonEmpty);
+        ow(c.firebase.databaseURL, "Configuration.firebase.databaseURL", ow.string.url);
+        ow(c.firebase.projectId, "Configuration.firebase.projectId", ow.string.nonEmpty);
+
+        FirestoreRolesConfiguration.validate(c.roles, "Configuration.roles ");
+    }
+
+    export function load(): Configuration {
+        if (!window.ROLES_CONFIGURATION) throw new Error("You must set the window.ROLES_CONFIGURATION env");
+        return window.ROLES_CONFIGURATION;
+    }
+}
+
+declare global {
+    interface Window { ROLES_CONFIGURATION: Configuration; }
+}
